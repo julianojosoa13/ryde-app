364d8a1f5736a730f2243bf99fc3702b67781be1
julianojosoa13 <josoajuliano@gmail.com>
2025-03-13 14:52:51 +0300

    Setup: Eslint & Prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
-import React, { FC } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
 
-interface Props {}
-
-const Index: FC<Props> = (props) => {
+const Index = () => {
   return (
     <SafeAreaView className="flex-1 h-screen w-screen justify-center items-center">
       <Text className="text-center mx-auto text-3xl text-red-600 font-bold">

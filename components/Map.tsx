ddddb64d84e79a3d9189f49bf662f0d6ef45c811
julianojosoa13 <josoajuliano@gmail.com>
2025-04-08 21:39:10 +0300

    Map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,22 @@
 import { View } from "react-native";
 
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
 const Map = () => {
-  return <View></View>;
+  // const region = {}
+
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showPointsOfInterests={false}
+      //   initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle={"light"}
+    ></MapView>
+  );
 };
 
 export default Map;
